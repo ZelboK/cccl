869717bef2d025c4d3b5d0c470d7b8930e5742ab
Danial Javady <danialjavady96@gmail.com>
2024-07-26 10:19:03 -0400

    save and push remote

--- libcudacxx/include/cuda/std/__cuda/semaphore.h
+++ libcudacxx/include/cuda/std/__cuda/semaphore.h
@@ -39,7 +39,32 @@ public:
 };
 
 template <thread_scope _Sco>
-using binary_semaphore = counting_semaphore<_Sco, 1>;
+using binary_semaphore2 = counting_semaphore<_Sco, 1>;
+
+template <thread_scope _Sco, ptrdiff_t __least_max_value = INT_MAX>
+struct __fair_semaphore {
+  cuda::atomic<unsigned int, _Sco> tickets{0};
+  cuda::atomic<unsigned int, _Sco> current{0};
+
+  _LIBCUDACXX_INLINE_VISIBILITY static constexpr ptrdiff_t max() noexcept {
+    return __least_max_value;  
+  }
+
+  void lock() {
+    auto t = tickets.fetch_add(1, cuda::memory_order_relaxed);
+    int wait_iterations = 1;
+    const int max_iterations = 1024;
+    while (t != current.load(cuda::memory_order_acquire)) {
+      for (int i = 0; i < wait_iterations; i++) { 
+      }
+      wait_iterations = min(wait_iterations * 2, max_iterations);
+    }
+  }
+  void unlock() { current.fetch_add(1, cuda::memory_order_release); }
+};
+
+template <thread_scope _Sco>
+using binary_semaphore = ::cuda::std::__fair_semaphore<_Sco, 1>;
 
 _LIBCUDACXX_END_NAMESPACE_CUDA
 
